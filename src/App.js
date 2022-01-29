fc1975bc2f7752387ea0367b1fcf28d207ab15ce
hp2002 <harshnilkanth2015@gmail.com>
2022-01-28 20:04:08 -0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import {useState} from 'react'
+import { useState } from 'react';
 import './style.css';
 
 export default function App() {
   const [count, setCount] = useState(0);
+  const [inputText, setInputText] = useState('');
 
-  return <div>
-    <p>{count}</p>
-    <button onClick={()=> setCount(count+1)}> Increment </button>
-  </div>;
+  return (
+    <div>
+      <p>{count}</p>
+      <input type="text"></input>
+      <button type="submit" onClick={() => setInputText()}>
+        Submit
+      </button>
+    </div>
+  );
 }
