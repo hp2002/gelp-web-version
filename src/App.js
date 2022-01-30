f0a7abec201757faedfd744410511292001a734c
hp2002 <harshnilkanth2015@gmail.com>
2022-01-30 13:08:37 -0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,16 +3,11 @@ import { useState } from 'react';
 import './style.css';
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const [inputText, setInputText] = useState('');
-
   return (
     <div>
       <p>{inputText}</p>
-      <input type="text" onChange ={()=> setInputText(inputText)}></input>
-      <button type="submit">
-        Submit
-      </button>
+      <input type="text" onChange={() => setInputText(inputText)}></input>
+      <button type="submit">Submit</button>
     </div>
   );
 }
