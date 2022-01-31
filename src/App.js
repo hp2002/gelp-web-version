e00dcdecb812c8ce0fad50f2ad07f0b6e0435d03
hp2002 <harshnilkanth2015@gmail.com>
2022-01-31 10:02:53 -0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import { useState } from 'react';
 import './style.css';
 
 export default function App() {
+  const [count, setCount] = useState;
+
+  function handleChange(e) {
+    const text = e.target.value;
+    setCount(text);
+  }
   return (
     <div>
-      <p>{inputText}</p>
-      <input type="text" onChange={() => setInputText(inputText)}></input>
-      <button type="submit">Submit</button>
+      <p>{count}</p>
+      <input type="text" onChange={handleChange}></input>
+      {/* <button type="submit">Submit</button> */}
     </div>
   );
 }
